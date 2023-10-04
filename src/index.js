6934cb7ac1f0cabc2508114366ece04599bdd3fd
dev1smiinii <sam.sangmin.jeon@gmail.com>
2023-10-04 13:14:03 +0900

    fix: index.js(minor moded) 수정

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const boardRouter = require("./board/board.route");
 
 router.get("/", (req, res) => {
   console.log(req.body);
+  console.log(`cookie: ${req.headers.cookie}`);
+  // console.log(`cookie: ${req.cookies}`) // npm install cookie-parser 이후 적용
   res.render("index.html");
 });
 
